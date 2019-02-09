64dff5b5e585ede69e05e9de7c091d22cca0ff1b
Wonnka <47060993+wonnka@users.noreply.github.com>
2019-02-09 15:36:02 -0200

    Create eval.js

---
+++ src/commands/admin/eval.js
@@ -0,0 +1,24 @@
+let utils = require("../../utils/constants")
+
+  module.exports = class {
+      constructor() {
+      
+           this.name = 'eval'
+           this.aliases - ['ev', 'code']
+           this.markdown = (type, cont) => "```" + type + "\n" + cont + "```"
+
+       }
+
+       run (client, msg, arg) {
+          try {
+               if (msg.author.id !== '501370728474476545') return;
+               let code = arg.join(" ").replace(/^```(js|javascript ?\n)?|```$/gi, "")
+               let out = eval(code)
+               if (typeof code !== 'string') code = require("util").inspect(code, { depht: 0 })
+               msg.channel.send(this.markdown("js", out))
+           } catch (err) {
+               msg.channel.send(this.markdown("js", err))
+           }
+       }
+
+   }
